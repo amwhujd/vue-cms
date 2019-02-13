e5aa25069f8da6507dc1864b7b79ee0c17f44c31
amwhujd <amwhujd@163.com>
2019-02-13 19:44:30 +0800

    首页轮播图和9宫格布局

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 // 入口文件
 import Vue from "vue"
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+//导入自已的router路由模块
+ import router from './router'
+
+//设置vue-resource的请求路径
+//Vue.http.options.root='http://www.lovegf.cn:8899/api/'
+//导入路由包
+import VueRouter from 'vue-router'
+
+//安装路由模块
+Vue.use(VueRouter)
 
 //引入mui的CSS样式文件
  import './lib/mui/css/mui.min.css'
 
  //引入mui的扩展字体的CSS文件
-//  import './lib/mui/css/icons-extra.css'
+    import './lib/mui/css/icons-extra.css'
 
 
 // 导入mint-UI组件
@@ -15,6 +28,8 @@ import 'mint-ui/lib/style.css'
 //安装mint-UI全局注册所有组件
 Vue.use(MintUI)
 
+
+
 //导入APP根组件 
 import app from './App.vue'
 
@@ -22,5 +37,6 @@ import app from './App.vue'
 
 var vm = new Vue({
     el:'#app',
-    render: c => c(app)
+    render: c => c(app),
+    router
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,23 @@
+import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+//导入对应的路由组件
+import homeComponent from './pages/home/index.vue'
+import memberComponent from './pages/member/index.vue'
+import shopcarComponent from './pages/shopcar/index.vue'
+import searchComponent from './pages/search/index.vue'
 
+ Vue.use(VueRouter)
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: homeComponent },
+    { path: '/member', component: memberComponent },
+    { path: '/shopcar', component: shopcarComponent },
+    { path: '/search', component: searchComponent },
+  ],
+  linkActiveClass: 'mui-active' //覆盖默认的路由高亮的类,默认的类叫做router-link-active
 })
 
 // 把路由对象暴露出去
